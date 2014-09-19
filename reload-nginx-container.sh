2af7b8db9ab20c21b9445cb9db8f49cfc967a7a3
andre <goroh@hotmail.com>
2014-09-19 01:15:49 -0300

    fig config

--- reload-nginx-container.sh
+++ reload-nginx-container.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-CID=$(docker inspect --format "{{ .Id }}" web)
-docker kill --signal="SIGHUP" $CID
+ 
+# RELOAD NGINX
+echo "Reloading nginx, dumping env"
+env
+# tries to detect if we are NOT running under fig
+if [[ "$PROXY_NAME" != "proxy_1" ]]
+then
+  # the form below does not work with fig
+  # because name is dynamic
+  CID=$(docker inspect --format "{{ .Id }}" $PROXY_NAME)
+  docker kill --signal="SIGHUP" $CID
+else
+  # the form below works with fig, but detects container
+  # name in an ugly way (whatever ends with "_proxy_1").
+  # don't blame me, fig makes funny names
+  FIGNAME=$(docker ps | grep "_proxy_1" | awk '{print $NF}')
+  CID=$(docker inspect --format "{{ .Id }}" $FIGNAME)
+  docker kill --signal="SIGHUP" $CID
+fi
 
